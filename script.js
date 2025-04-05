6f6a04e1f4625c1dc5174a597c548ee1a07c0ef3
Karan Jangid <karanjangid198@gmail.com>
2025-04-05 08:43:00 +0530

    upload code

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigation=document.querySelector("nav")
+let navigationIcon=document.querySelector("#navIcon")
+
+navigationIcon.addEventListener("click", ()=>{
+    navigation.classList.toggle("mIcon")
+})
