9391eefdd5fdf5945408fd59e2f8c5d84c58b716
Karan Jangid <karanjangid198@gmail.com>
2025-04-08 09:28:56 +0530

    add sections and images

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ let navigationIcon=document.querySelector("#navIcon")
 navigationIcon.addEventListener("click", ()=>{
     navigation.classList.toggle("mIcon")
 })
+
+
+// FAQ script 
+
+let outerFaq=document.querySelectorAll(".faq h3");
+
+outerFaq.forEach((items,index)=>{
+    items.addEventListener("click", ()=>{
+
+        items.nextElementSibling.classList.toggle("faqOutputShow")
+
+        outerFaq.forEach((innerItems,innerIndex)=>{
+            if(innerIndex != index){
+                innerItems.nextElementSibling.classList.remove("faqOutputShow")
+                innerItems.firstElementChild.innerHTML= "-"
+            }
+        })
+
+        if(items.firstElementChild.innerHTML== "-"){
+            items.firstElementChild.innerHTML= "+"
+        }
+        else{
+            items.firstElementChild.innerHTML= "-"
+        }
+    })
+});
+
+
+
+
+
